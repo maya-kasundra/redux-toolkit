a9b0e64b942fd3c65d2851e9c8d0cdb27c577bfb
Maya Kasundra <maya.k@immence.in>
2023-02-02 13:37:17 +0530

    axios-api-fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const styleObj = {
+    color: "#4a54f1",
+    textAlign: "center",
+    fontSize: "30px",
+}
+  const [myData, setMyData]= useState([]);
+  const [isError, setIsError]= useState('');
+  useEffect(()=>{
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+    .then((res)=>setMyData(res.data))
+    .catch((error)=>setIsError(error.message))
+  },[])
   return (
-    <div>hi</div>
+    <>
+    <h1 style={styleObj}>Axios Tutorial</h1>
+    {isError !== "" &&  <h2>{isError}</h2>}
+    <div className="grid">
+      {myData.map((post)=>{
+        const {id,title,body} = post
+          return(
+            <div className="card" key={id}>
+              <h2>{title.slice(0,9).toUpperCase()}</h2>
+              <p>{body}</p>
+            </div>
+          )
+      })}
+    </div>
+    <div>{myData.id}</div>
+    </>
   );
 }
 
